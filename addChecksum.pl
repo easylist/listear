50c87b750cd0840f6085018089df5ccb614338ef
SMed79 <none@none>
2019-04-23 15:13:05 +0100

    M: Update addChecksum.pl

--- addChecksum.pl
+++ addChecksum.pl
@@ -1,6 +1,7 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 # Copyright 2011 Wladimir Palant
+# https://palant.de/
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,10 +16,9 @@
 # limitations under the License.
 
 #############################################################################
-# This is a slightly modified version of the reference script to add        #
-# checksums to downloadable subscriptions. The checksum will be validated   #
-# by Adblock Plus on download and checksum mismatches(broken downloads)     #
-# will be rejected.															#
+# Script to add checksums to downloadable subscriptions. The checksum will  #
+# be validated by Adblock Plus on download and checksum mismatches          #      
+# (broken downloads) will be rejected.                                      #
 #                                                                           #
 # To add a checksum to a subscription file, run the script like this:       #
 #                                                                           #
@@ -26,86 +26,60 @@
 #                                                                           #
 # Note: your subscription file should be saved in UTF-8 encoding, otherwise #
 # the generated checksum might be incorrect.                                #
-#                                                                           #
+#                                                      AdblockPlus.Org      #
 #############################################################################
 
 use strict;
 use warnings;
+use Path::Tiny;
 use Digest::MD5 qw(md5_base64);
+use Encode qw(encode_utf8);
+use POSIX qw(strftime);
+use feature 'unicode_strings';
 
 die "Usage: $^X $0 subscription.txt\n" unless @ARGV;
 
-#my $file = $ARGV[0];
-foreach my $file (@ARGV) {
-  my $data = readFile($file);
+my $file = shift;
 
-  # Get existing checksum.
-  $data =~ /^.*!\s*checksum[\s\-:]+([\w\+\/=]+).*\n/gmi;
-  my $oldchecksum = $1;
+die "Specified file: $file doesn't exist!\n" unless (-e $file);
 
-  # Remove already existing checksum.
-  $data =~ s/^.*!\s*checksum[\s\-:]+([\w\+\/=]+).*\n//gmi;
+my $data = path($file)->slurp_utf8;
 
-  # Calculate new checksum: remove all CR symbols and empty
-  # lines and get an MD5 checksum of the result (base64-encoded,
-  # without the trailing = characters).
-  my $checksumData = $data;
-  $checksumData =~ s/\r//g;
-  $checksumData =~ s/\n+/\n/g;
+# Get existing checksum
+$data =~ /^.*!\s*checksum[\s\-:]+([\w\+\/=]+).*\n/gmi;
+my $oldchecksum = $1;
 
-  # Calculate new checksum
-  my $checksum = md5_base64($checksumData);
+# Remove already existing checksum
+$data =~ s/^.*!\s*checksum[\s\-:]+([\w\+\/=]+).*\n//gmi;
 
-  # If the old checksum matches the new one bail.
-  if ($checksum eq $oldchecksum)
-  {
-    $data = ();
-	  next;
-  }
+# Calculate new checksum: remove all CR symbols and empty
+# lines and get an MD5 checksum of the result (base64-encoded,
+# without the trailing = characters)
+my $checksumData = $data;
+$checksumData =~ s/\r//g;
+$checksumData =~ s/\n+/\n/g;
+
+# Calculate new checksum
+my $checksum = md5_base64(encode_utf8($checksumData));
+
+# If the old checksum matches the new one die
+die "List has not changed.\n" if (($oldchecksum) and ($checksum eq $oldchecksum));
 
 # Update the date and time.
-my @months = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
-my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
-$year += 1900; # Year is years since 1900.
-my $ampm = "AM";
-if ($hour >= 12)    {$ampm = "PM";}
-if ($hour == 0)     {$hour = 12;  }
-elsif ($hour >= 13) {$hour -= 12; }
-my $dat = sprintf("$mday $months[$mon] $year (%d:%02d:%02d $ampm)", $hour, $min, $sec);
-$data =~ s/(!.*Updated:\s*)([^\r\n]*)(\r?\n)/$1$dat$3/;
+my $updated = strftime("%Y-%m-%d %H:%M UTC", gmtime);
+$data =~ s/(^.*!.*(Last modified|Updated):\s*)(.*)\s*$/$1$updated/gmi if ($data =~ m/^.*!.*(Last modified|Updated)/gmi);
 
-  # Recalculate the checksum as we've altered the date.
-  $checksumData = $data;
-  $checksumData =~ s/\r//g;
-  $checksumData =~ s/\n+/\n/g;
-  $checksum = md5_base64($checksumData);
+# Update version
+my $version = strftime("%Y%m%d%H%M" ,gmtime);
+$data =~ s/^.*!\s*Version:.*/! Version: $version/gmi;
 
-  # Insert checksum into the file
-  $data =~ s/(\r?\n)/$1! Checksum: $checksum$1/;
+# Recalculate the checksum as we've altered the date
+$checksumData = $data;
+$checksumData =~ s/\r//g;
+$checksumData =~ s/\n+/\n/g;
+$checksum = md5_base64(encode_utf8($checksumData));
 
-  writeFile($file, $data);
-  $data = ();
-}
+# Insert checksum into the file
+$data =~ s/(\r?\n)/$1! Checksum: $checksum$1/;
 
-sub readFile
-{
-  my $file = shift;
-
-  open(local *FILE, "<", $file) || die "Could not read file '$file'";
-  binmode(FILE);
-  local $/;
-  my $result = <FILE>;
-  close(FILE);
-
-  return $result;
-}
-
-sub writeFile
-{
-  my ($file, $contents) = @_;
-
-  open(local *FILE, ">", $file) || die "Could not write file '$file'";
-  binmode(FILE);
-  print FILE $contents;
-  close(FILE);
-}
+path($file)->spew_utf8($data);
